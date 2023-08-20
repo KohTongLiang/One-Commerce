8fdf1e2d0e71b0425581edb44e93cde1950934ac
KohTongLiang <koh.tongliang@gmail.com>
2023-08-20 13:42:46 +0800

    Note Taking sample
    * Completed Adding notes feature
    * Added server sent event to update ui with list of notes once a new
    noteis added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import rateLimit from 'express-rate-limit';
-import DailyRotateFile from 'winston-daily-rotate-file';
-import winston from 'winston';
 import path = require('path');
+import { EventEmitter } from 'events';
 
 dotenv.config(); // Load environment variables
 
@@ -24,27 +22,34 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'pug');
 
+/// Some vars
+let notesCache = new Array();
+const updateEmitter = new EventEmitter();
+
 /// Routes for testing
 app.get('/', (req, res) => {
   return res.render('index', { 
     title: 'Home', 
-    getTestUrl: 'http://localhost:3000/test', 
-    body: 'This the paragraph text.'
+    formSubmitUrl: 'http://localhost:3000/submit',
+    sseUrl: 'http://localhost:3000/sse',
+    getFooterUrl: 'http://localhost:3000/test', 
+    footerText: 'This is a footer text from the server.'
   });
 });
 
 app.get('/test', (req, res) => {
-  return res.send({ body: new Date() });
+  return res.send(`<span>${new Date()}</span>`);
 });
 
 app.post("/submit", (req, res) => {
   console.log(`Note submission: ${req.body.title} & ${req.body.description}`)
-  return res.send("Okay")
+  notesCache.push({ title: req.body.title, description: req.body.description })
+  updateEmitter.emit("updateEvent");
+  return res.send("<em class='text-yellow-500'>Successfully Submitted</em>")
 })
 
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, '..', 'src', 'views'))
-
 
 /// Server sent event
 // Route for the server-sent event source
@@ -56,57 +61,19 @@ app.get('/sse', (req, res) => {
   // Send a comment to the client to establish the connection
   res.write(': connected\n\n');
 
-  // Send a message to the client every 5 seconds
-  const intervalId = setInterval(() => {
-    const data = { message: 'Hello, world!' };
-    res.write(`data: ${JSON.stringify(data)}\n\n`);
-  }, 5000);
+  // Send a message to the client when cache is updated
+  updateEmitter.on("updateEvent", () => {
+    console.log("Fired Update Event");
+    let htmlResult = "<div>";
+    notesCache.forEach((value) => {
+      htmlResult += `<p>Title: ${value.title}</p> <p>Description: ${value.description}</p>`;
+    });
+    htmlResult += "</div>";
+    res.write(`data: ${htmlResult} \n\n`);
+  });
 
   // Handle client disconnections
   req.on('close', () => {
-    clearInterval(intervalId);
     res.end();
   });
-});
-
-// setup rate-limiting
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 100, // 100 requests per minute
-});
-app.use(limiter);
-
-// setup logger
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    winston.format.errors({ stack: true }),
-    winston.format.splat(),
-    winston.format.json(),
-  ),
-  transports: [
-    new DailyRotateFile({
-      filename: 'logs/application-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
-
-// log requests
-app.use((req, res, next) => {
-  logger.info({
-    message: 'Request URL',
-    url: req.url,
-    method: req.method,
-    query: req.query,
-    headers: req.headers,
-    body: req.body,
-  });
-  next();
 });
