af373bd87937aa4f5d1c7abb0582417bc0b2e8f4
Koh Tong Liang <koh.tongliang@gmail.com>
2024-06-23 22:20:29 +0800

    Content/Styling Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,pug}"],
+  content: ['./src/**/*.{html,js,pug}'],
   theme: {
-      colors: {
-          'background' : '#F8F0E5',
-          'primary': '#DAC0A3',
-          'secondary': '#EADBC8',
-          'text': '#0F2C59',
-          'text-normal': '#000000'
-      },
-      extend: {},
+    colors: {
+      background: '#F8F0E5',
+      primary: '#DAC0A3',
+      secondary: '#EADBC8',
+      text: '#0F2C59',
+      'text-normal': '#000000',
+      white: '#ffffff',
+    },
+    extend: {},
   },
   plugins: [],
-}
+};
